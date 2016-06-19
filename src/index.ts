f760c9a289b52deb63636ca0139f339606a87a95
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 23:50:54 +0200

    feat(all): initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import {WebpackConfig, get} from '@easy-webpack/core'
+const OfflinePlugin = require('offline-plugin')
+
+/**
+ * Plugin: OfflinePlugin
+ * Description: This plugin is intended to provide offline experience for webpack projects. It uses ServiceWorker and AppCache as a fallback under the hood.
+ * It always better if OfflinePlugin is the last plugin added
+ * 
+ * See: https://github.com/NekR/offline-plugin
+ */
+export function offline(options?: any) {
+  return function offline(this: WebpackConfig): WebpackConfig {
+    return {
+      plugins: get(this, 'plugins', []).concat([
+        new OfflinePlugin(options),
+      ])
+    }
+  }
+}
